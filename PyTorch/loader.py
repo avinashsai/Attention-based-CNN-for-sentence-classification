4792abece4e3178499adc46907719a6a81e8fb3b
Avinash Madasu <avinash.sai001@gmail.com>
2019-09-17 23:11:22 +0530

    attention cnn

--- PyTorch/loader.py
+++ PyTorch/loader.py
@@ -8,9 +8,9 @@ from convert import *
 datapath = '../Datasets/'
 
 def preprocess(text):
-  text = text.translate(str.maketrans('', '', string.punctuation))
-  words = text.split()
-  return " ".join(word.lower() for word in words)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    words = text.split()
+    return " ".join(word.lower() for word in words)
 
 def load_trec(senlen):
     lab = {'DESC':0,'ENTY':1,'ABBR':2,'HUM':3,'LOC':4,'NUM':5}
@@ -67,6 +67,85 @@ def load_mr(senlen):
 
     traincorpus,testcorpus,trainlabels,testlabels = train_test_split(corpus,labels,test_size=0.2,random_state=42)
     traincorpus,valcorpus,trainlabels,vallabels = train_test_split(traincorpus,trainlabels,test_size=0.1,random_state=42)
+
+    vocab = get_vocab(traincorpus)
+    trainloader = get_loader(traincorpus,trainlabels,vocab,senlen)
+    valloader = get_loader(valcorpus,vallabels,vocab,senlen)
+    testloader = get_loader(testcorpus,testlabels,vocab,senlen)
+
+    embedmatrix = get_embeddings(vocab)
+
+    return trainloader,valloader,testloader,embedmatrix
+
+
+def load_sst2(senlen):
+    traincorpus = []
+    trainlabels = []
+    with open(datapath+'SST-2/'+'train.txt','r',encoding='latin1') as f:
+        for line in f.readlines():
+            words = line.split()
+            trainlabels.append(int(words[0]))
+            traincorpus.append(preprocess(" ".join(word for word in words[1:])))
+    
+    valcorpus = []
+    vallabels = []
+    with open(datapath+'SST-2/'+'dev.txt','r',encoding='latin1') as f:
+        for line in f.readlines():
+            words = line.split()
+            vallabels.append(int(words[0]))
+            valcorpus.append(preprocess(" ".join(word for word in words[1:])))
+
+
+    testcorpus = []
+    testlabels = []
+    with open(datapath+'SST-2/'+'test.txt','r',encoding='latin1') as f:
+        for line in f.readlines():
+            words = line.split()
+            testlabels.append(int(words[0]))
+            testcorpus.append(preprocess(" ".join(word for word in words[1:])))
+
+    trainlabels = np.asarray(trainlabels)
+    vallabels = np.asarray(vallabels)
+    testlabels = np.asarray(testlabels)
+
+    vocab = get_vocab(traincorpus)
+    trainloader = get_loader(traincorpus,trainlabels,vocab,senlen)
+    valloader = get_loader(valcorpus,vallabels,vocab,senlen)
+    testloader = get_loader(testcorpus,testlabels,vocab,senlen)
+
+    embedmatrix = get_embeddings(vocab)
+
+    return trainloader,valloader,testloader,embedmatrix
+
+def load_sst5(senlen):
+    traincorpus = []
+    trainlabels = []
+    with open(datapath+'SST-5/'+'train.txt','r',encoding='latin1') as f:
+        for line in f.readlines():
+            words = line.split()
+            trainlabels.append(int(words[0]))
+            traincorpus.append(preprocess(" ".join(word for word in words[1:])))
+    
+    valcorpus = []
+    vallabels = []
+    with open(datapath+'SST-5/'+'dev.txt','r',encoding='latin1') as f:
+        for line in f.readlines():
+            words = line.split()
+            vallabels.append(int(words[0]))
+            valcorpus.append(preprocess(" ".join(word for word in words[1:])))
+
+
+    testcorpus = []
+    testlabels = []
+    with open(datapath+'SST-5/'+'test.txt','r',encoding='latin1') as f:
+        for line in f.readlines():
+            words = line.split()
+            testlabels.append(int(words[0]))
+            testcorpus.append(preprocess(" ".join(word for word in words[1:])))
+
+    trainlabels = np.asarray(trainlabels)
+    vallabels = np.asarray(vallabels)
+    testlabels = np.asarray(testlabels)
 
     vocab = get_vocab(traincorpus)
     trainloader = get_loader(traincorpus,trainlabels,vocab,senlen)
--- PyTorch/main.py
+++ PyTorch/main.py
@@ -26,7 +26,7 @@ torch.manual_seed(seed)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-da','--dataset',type=str,help='dataset(trec or mr)',default='trec')
+    parser.add_argument('-da','--dataset',type=str,help='dataset(trec or mr or sst5 or sst2)',default='trec')
     parser.add_argument('-ru','--runs',type=int,help='number of runs',default=10)
     parser.add_argument('-lam','--lamda',type=float,help='distance decay',default=0.0)
 
@@ -43,6 +43,17 @@ def main():
         trainloader,valloader,testloader,embedmatrix = load_mr(senlen)
         testacc = trainmodel(senlen,embedmatrix,numclasses,args.lamda,args.runs,trainloader,valloader,testloader)
         print("Accuracy on MR Dataset {} ".format(testacc))
-   
+    elif(args.dataset=='sst5'):
+    	senlen = 20
+    	numclasses = 5
+		trainloader,valloader,testloader,embedmatrix = load_sst5(senlen)
+		testacc = trainmodel(senlen,embedmatrix,numclasses,args.lamda,args.runs,trainloader,valloader,testloader)
+		print("Accuracy on SST-5 Dataset {} ".format(testacc))
+	elif(args.dataset=='sst2'):
+		senlen = 20
+		numclasses = 2
+		trainloader,valloader,testloader,embedmatrix = load_sst2(senlen)
+		testacc = trainmodel(senlen,embedmatrix,numclasses,args.lamda,args.runs,trainloader,valloader,testloader)
+		print("Accuracy on SST-2 Dataset {} ".format(testacc))
 if __name__ == "__main__":
     main()
